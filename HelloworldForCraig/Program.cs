fbd89e6aec426dd3b094146fd707bd4f6336a2a9
John Oscar Molteberg <johnoscarm@gmail.com>
2024-01-18 11:57:27 +0100

    Add method counting to 10

--- HelloworldForCraig/Program.cs
+++ HelloworldForCraig/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string name = "John";
 Console.WriteLine($"Hello, {name}!");
+
+
+Console.WriteLine(string.Join(" ", CountTo(10)));
+
+static List<int> CountTo(int num)
+{
+    List<int> numbers = [];
+    for (int i = 1; i <= num; i++)
+    {
+        numbers.Add(i);
+    }
+    return numbers;
+}
