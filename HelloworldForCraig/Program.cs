1b1df546a692511b04f6e438c6225ca8e5d00eac
John Oscar Molteberg <johnoscarm@gmail.com>
2024-01-18 11:58:48 +0100

    Add some comments

--- HelloworldForCraig/Program.cs
+++ HelloworldForCraig/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 string name = "John";
+// Print my name to the console
 Console.WriteLine($"Hello, {name}!");
 
-
+// Run method
 Console.WriteLine(string.Join(" ", CountTo(10)));
 
+// Method counting to 10
 static List<int> CountTo(int num)
 {
     List<int> numbers = [];
